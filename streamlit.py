521278827af8ede9240f5ead5868f5de4c39343c
joesu777 <jswan4123@gmail.com>
2025-09-30 21:51:20 +0700

    Added the pkl, requirements, and streamlit script

---
+++ streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from fastai.vision.all import *
+
+st.title("Pet Breed Classifier")
+st.text("Built by Joel Suwanto")
+
+#Homework:
+#Load the pickle file
+#make a new predict function
+#In the prediction function, create a new PILImage
+#Make a new file_uploader (exact same as previous project)
+
+
