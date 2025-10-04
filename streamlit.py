42c81a415e218dd469e7d6c9970fb1467940455f
joesu777 <jswan4123@gmail.com>
2025-10-04 09:55:56 +0700

    Added extract_breed_name function

--- streamlit.py
+++ streamlit.py
@@ -2,29 +2,37 @@ import streamlit as st
 from fastai.vision.all import *
 
 
-# Load the pre-trained breed model
+def extract_breed_name(file_name):
+    p = Path(file_name)
+    breed_name_parts = p.stem.split("_")
+    final_breed_name = ""
+    # breed_name_parts.pop()
+    length_parts = len(breed_name_parts) - 1
+    for i in range(length_parts):
+        final_breed_name += breed_name_parts[i]
+        if i != length_parts - 1:
+            final_breed_name += "_"
+
+    return final_breed_name
+
 breed_model = load_learner("cat_dog_breed_model.pkl")
 
 
-# Define prediction function
 def predict(image):
     img = PILImage.create(image)
     pred_class, pred_idx, outputs = breed_model.predict(img)
-    return pred_class  # For breed classification, just return the predicted class
+    return pred_class
 
 
 # Streamlit UI
 st.title("Pet Breed Classifier")
 st.text("Built by Joel Suwanto")
 
-# File uploader
 uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    # Display uploaded image
     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-    # Prediction
     prediction = predict(uploaded_file)
     st.subheader(f"Predicted Breed: {prediction}")
 
